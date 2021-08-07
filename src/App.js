85f1c69569122caea26ed0fc44b091333b6f08da
malasia-415 <malasia415@gmail.com>
2021-08-07 15:58:25 -0700

    create a myths container that will hold the fetched myths

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
+import MythsContainer from './Containers/MythsContainer'
+import './style.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <center>
-        <h1>The New And Improved Kamala Facts 2 Application</h1>
-        <h3>
-          This application will be built off of React with a Rails API. <br></br>The Rails API repo is here:
-          <a href ="https://github.com/Sydnee510/kamala_facts_2_backend" target ="_blank" rel="noopener noreferrer"> API Kamala Facts 2 </a>
-           <br></br>Clone it so you can see how it operates and lets get creative! 
-        </h3>
-        
-        <h2>DONT GET ME CONFUSED WITH KAMALA FACTS 1 <br></br>
-         <a href ="https://kamalafacts.com/" target ="_blank"  rel="noopener noreferrer">Remember Me</a> 
-        </h2>
+        <MythsContainer/>
         </center>
       </div>
     );
