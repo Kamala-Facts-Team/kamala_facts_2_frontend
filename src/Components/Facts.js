aa860b007e4879328e69e182192254f91679651d
malasia-415 <malasia415@gmail.com>
2021-08-07 16:02:50 -0700

    create a facts component

---
+++ src/Components/Facts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Facts extends React.Component {
+    render() {
+      return (
+        <div className="App">
+          <center>
+          <h1>The Facts will render here</h1>
+          </center>
+        </div>
+      );
+    } 
+    
+  }
+  
+  export default Facts;
+  
