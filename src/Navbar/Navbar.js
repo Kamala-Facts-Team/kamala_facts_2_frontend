448970a51ab00bd8eb4dddbf3d9f47284de67ab1
sydnee510 <sbsydnee161416@gmail.com>
2021-08-29 15:15:44 -0700

    added a NavSearchBar component for search in nav bar, also add class names to each nav link

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,27 +1,22 @@
 import React from "react";
-import SearchBar from "../Components//Search/SearchBar";
-import { Nav, NavLink, Bars, NavMenu } from "../Navbar/NavbarElements";
+import NavSearchBar from "../Components/Search/NavSearchBar";
+import {NavLink} from "../Navbar/NavbarElements";
 import "./nav.css"
 
 export default function Navbar() {
     return (
         <div className="main_div">
-                <div className="main_div1">
-
-                <h1>Kamala Facts</h1>
-                <button className="cont-Button">become a contributor</button>
-                </div>
-                <div className="main_div2">
-                <SearchBar />
-                </div >
-                    {/* <Bars /> */}
-                 
-                        {/* <NavLink to="/" exact activeStyle>
-                            Home
-                        </NavLink>
-                        <NavLink to="/about" activeStyle>
-                            About
-                        </NavLink> */}
+               
+            <NavLink className="nav-link" to="/" exact activeStyle>
+                Home
+            </NavLink>
+            <NavLink className="nav-link" to="/about" activeStyle>
+                About
+            </NavLink>
+            
+            <NavSearchBar/>
+          
         </div>
+        
     );
 }
