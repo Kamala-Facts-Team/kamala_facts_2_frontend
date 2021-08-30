a77be91f600f2db364d1cc9afab19f04cc560d03
Your Name <you@example.com>
2021-08-29 20:49:52 -0500

    #25 fixed search bar

--- src/Components/Myths/Myths.js
+++ src/Components/Myths/Myths.js
@@ -3,59 +3,65 @@ import PageSearchBar from "../Search/PageSearchBar";
 import Notes from "../../Notes";
 
 export default function Myths() {
-  // usestate
-  const [myths, setMyths] = useState({});
-  const [Loading, setLoading] = useState(false);
+    // usestate
+    const [myths, setMyths] = useState({});
+    const [Loading, setLoading] = useState(false);
+    const [inputData, setInputData] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://kamala-facts-2-backend.herokuapp.com/api/v1/myths"
-      );
-      const data = await response.json();
-      setMyths(data);
-      setLoading(true);
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://kamala-facts-2-backend.herokuapp.com/api/v1/myths");
+            const data = await response.json();
+            setMyths(data);
+            setLoading(true);
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  console.log("hii");
+    console.log("hii");
 
-  console.log(myths);
-  console.log(Loading)
-  ;
+    console.log(myths);
+    console.log(Loading);
 
+    // im mapping over object values, then mapping over nested array values(Marlo)
+    const mapMyths = Object.values(myths)
+        .filter((item) => {
+            let thing;
+            if (!inputData) {
+                thing = item;
+            } else if (item.lie_statement.toLocaleLowerCase().includes(inputData.toLocaleLowerCase())) {
+                thing = item;
+            }
+            return thing;
+        })
+        .map((item) => {
+            return (
+                <div key={item.id}>
+                    <h3>`Myth: ${item.lie_statement}`</h3>
+                    {item.facts.map((fact) => {
+                        return <p>`Truth: {fact.truth_statement}`</p>;
+                    })}
+                </div>
+            );
+        });
 
-  // im mapping over object values, then mapping over nested array values(Marlo)
-  const mapMyths = Object.values(myths).map((item) => {
     return (
-      <div key={item.id}>
-        <h3>`Myth: ${item.lie_statement}`</h3>
-        {item.facts.map((fact) => {
-          return <p>`Truth: {fact.truth_statement}`</p>;
-        })}
-      </div>
-    );
-  });
+        <div className="App">
+            <center>
+                <Notes />
+                <img
+                    class="Kamala-pic-1"
+                    src="https://www.gannett-cdn.com/presto/2019/12/05/USAT/751f29ad-47cd-4791-9506-d4b6b846b53e-KamalaHarris.jpg"
+                    alt="Kamala Harris"
+                />
 
-  return (
-    <div className="App">
-      <center>
-        <Notes />
-        <img
-          class="Kamala-pic-1"
-          src="https://www.gannett-cdn.com/presto/2019/12/05/USAT/751f29ad-47cd-4791-9506-d4b6b846b53e-KamalaHarris.jpg"
-          alt="Kamala Harris"
-        />
-        
-      
-        <PageSearchBar />
-        
-        <h1>The myths will render here </h1>
-        {Loading ? mapMyths : <div>Loading</div>}
-        {/* {mapMyths} */}
-      </center>
-    </div>
-  );
+                <PageSearchBar setInputData={setInputData} inputData={inputData} />
+                <p>{inputData}</p>
+
+                <h1>The myths will render here </h1>
+                {Loading ? mapMyths : <div>Loading</div>}
+            </center>
+        </div>
+    );
 }
--- src/Components/Search/PageSearchBar.js
+++ src/Components/Search/PageSearchBar.js
@@ -1,15 +1,31 @@
-import React from "react";
-import "./search.css"
+import React, { useState } from "react";
+import "./search.css";
 
-export default function PageSearchBar() {
+export default function PageSearchBar(props) {
+    const handleSubmit = (event) => {};
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        props.setInputData(value);
+    };
+
     return (
         <div className="search_div">
             <center>
-                <form  className="page-searchbar" action="/" method="get">
+                <form className="page-searchbar" onSubmit={handleSubmit}>
                     <label htmlFor="header-search">
                         <span className="visually-hidden">Search Myths </span>
                     </label>
-                    <input type="text" id="header-search" placeholder="Search Myths" name="s" />
+
+                    <input
+                        type="text"
+                        id="header-search"
+                        placeholder="Search Myths"
+                        name="s"
+                        value={props.inputData}
+                        onChange={handleChange}
+                    />
+
                     <button type="submit">Search</button>
                 </form>
             </center>
