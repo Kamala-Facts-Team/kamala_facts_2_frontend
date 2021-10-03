d38e4a3eb17ddf5e645f55387dda46d8ee4ecbfe
Marlo Jones <marlo@Marlos-MacBook-Pro.local>
2021-10-03 16:03:06 -0400

    add from macbook

--- src/pages/Home/Myths.js
+++ src/pages/Home/Myths.js
@@ -5,109 +5,136 @@ import useStore from "../../store";
 import "./myths.css";
 
 export default function Myths() {
-    const addarraytomyths = useStore((state) => state.addarraytomyths);
-    const mythsarray = useStore((state) => state.myths);
+  const addarraytomyths = useStore((state) => state.addarraytomyths);
+  const mythsarray = useStore((state) => state.myths);
 
-    // console.log(mythsarray[0]);
+  // console.log(mythsarray[0]);
 
-    // usestate
-    const [myths, setMyths] = useState({});
-    const [Loading, setLoading] = useState(false);
-    const [inputData, setInputData] = useState("");
+  // usestate
+  const [myths, setMyths] = useState({});
+  const [Loading, setLoading] = useState(false);
+  const [inputData, setInputData] = useState("");
+  const [selectedItem, setSelectedItem] = useState(false);
+  const [selectedID, SetSelectedID] = useState();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("https://kamala-facts-2-backend.herokuapp.com/api/v1/myths");
-            const data = await response.json();
-            setMyths(data);
-            setLoading(true);
-            addarraytomyths(data);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://kamala-facts-2-backend.herokuapp.com/api/v1/myths"
+      );
+      const data = await response.json();
+      setMyths(data);
+      setLoading(true);
+      addarraytomyths(data);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    // Maps over object values from the api and filters by lie statment depending
-    // on what is typed in the search bar
-    const mapMyths = Object.values(myths)
-        .filter((item) => {
-            let filteredItem;
-            const commonWords = [
-                "a",
-                "and",
-                "the",
-                "you",
-                "can",
-                "is",
-                "it",
-                "if",
-                "we",
-                "need",
-                "in",
-                "us",
-                "this",
-                "their",
-                "they",
-                "to",
-                "kamala",
-                "harris",
-                "into",
-            ];
+  const ChosenStyle = {
+    border: "4px solid Yellow",
+  };
+  const regularStyle = {
+    border: "4px solid grey",
+  };
 
-            let filteredInput = inputData
-                .toLocaleLowerCase()
-                .split(" ")
-                .filter((word) => {
-                    if (!commonWords.includes(word)) {
-                        return word;
-                    } else {
-                        return null;
-                    }
-                });
+  function selectedLie(id,event){
+      event.preventDefault();
+    SetSelectedID(id);
+    console.log(id);
+    
+  }
+ 
 
-            let filteredLie = item.lie_statement
-                .toLocaleLowerCase()
-                .split(" ")
-                .filter((word) => {
-                    if (!commonWords.includes(word)) {
-                        return word;
-                    } else {
-                        return null;
-                    }
-                });
+  // Maps over object values from the api and filters by lie statment depending
+  // on what is typed in the search bar
+  const mapMyths = Object.values(myths)
+    .filter((item) => {
+      let filteredItem;
+      const commonWords = [
+        "a",
+        "and",
+        "the",
+        "you",
+        "can",
+        "is",
+        "it",
+        "if",
+        "we",
+        "need",
+        "in",
+        "us",
+        "this",
+        "their",
+        "they",
+        "to",
+        "kamala",
+        "harris",
+        "into",
+      ];
 
-            if (!inputData) {
-                filteredItem = item;
-            } else if (filteredInput.every((word) => filteredLie.includes(word))) {
-                filteredItem = item;
-            } else {
-                return null;
-            }
-
-            return filteredItem;
-        })
-        .map((item) => {
-            return (
-                <div key={item.id}>
-                    <MythsContent key={item.id} id={item.id} myth={item.lie_statement} />
-                </div>
-            );
+      let filteredInput = inputData
+        .toLocaleLowerCase()
+        .split(" ")
+        .filter((word) => {
+          if (!commonWords.includes(word)) {
+            return word;
+          } else {
+            return null;
+          }
         });
 
-    // console.log(myths);
+      let filteredLie = item.lie_statement
+        .toLocaleLowerCase()
+        .split(" ")
+        .filter((word) => {
+          if (!commonWords.includes(word)) {
+            return word;
+          } else {
+            return null;
+          }
+        });
 
-    return (
-        <div className="home_container">
-            <center>
-                <h1 className="title">Kamala's Fact Search Engine</h1>
+      if (!inputData) {
+        filteredItem = item;
+      } else if (filteredInput.every((word) => filteredLie.includes(word))) {
+        filteredItem = item;
+      } else {
+        return null;
+      }
 
-                <PageSearchBar setInputData={setInputData} inputData={inputData} />
-
-                <div className="outer-Div">
-                    {/* <h1>The myths will render here </h1> */}
-                    {Loading ? mapMyths : <div>Loading</div>}
-                </div>
-            </center>
+      return filteredItem;
+    })
+    .map((item) => {
+      return (
+        <div key={item.id} style={item.id == selectedID  ? ChosenStyle : regularStyle}>
+          {/* <div style={ChosenStyle} key={item.id}>  */}
+          <MythsContent
+            click={() => selectedLie(item.id)}//////the page is reloading right a function to prevent it
+            // click ={setTimeout(() => selectedLie(item.id), 2000)}
+            key={item.id}
+            id={item.id}
+            myth={item.lie_statement}
+          />
         </div>
-    );
+      );
+    });
+    console.log(selectedID);
+
+  // console.log(myths);
+
+  return (
+    <div className="home_container">
+      <center>
+        <h1 className="title">Kamala's Fact Search Engine</h1>
+
+        <PageSearchBar setInputData={setInputData} inputData={inputData} />
+
+        <div className="outer-Div">
+          {/* <h1>The myths will render here </h1> */}
+          {Loading ? mapMyths : <div>Loading</div>}
+        </div>
+      </center>
+    </div>
+  );
 }
--- src/pages/Home/MythsContent.js
+++ src/pages/Home/MythsContent.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React ,{ useState }from "react";
 
 import { Link } from "react-router-dom";
 import './mythcontent.css'
 
 export default function MythsContent(props) {
+
   return (
     
       <div className="inner-div">
